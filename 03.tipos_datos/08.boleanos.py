b5778c1494d72a5f7916c30eb9c2bfb6bbd02dbf
AndersonQuispeChipana <andersonchipany@gmail.com>
2026-05-22 10:17:02 -0500

    clase de boleanos y practica

---
+++ 03.tipos_datos/08.boleanos.py
@@ -0,0 +1,10 @@
+asistencia:bool=True # si asistio
+is_active:bool=False
+# cuando la variable se declara por primmera vez se tiene que indicar el tipo de dato
+asistencia=1
+is_active=0
+# cuando reeutilizamos la variable ya mo es necesario indicar el tipo de dato.
+print(asistencia)
+
+# mutabilidad de datos
+## se refirere que en python le podemos cambiar de valor a las variables
